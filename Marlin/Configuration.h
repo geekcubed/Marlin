e90f60473a1bef31b61cf8a2025ee6cd7e8abdd9
Ian Stapleton <ian@geekcubed.org>
2020-04-29 10:14:38 +0100

    Enable PID based bed heating

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -198,7 +198,7 @@
 // If your configuration is significantly different than this and you don't understand the issues involved, you probably
 // shouldn't use bed PID until someone else verifies your hardware works.
 // If this is enabled, find your own PID constants below.
-//#define PIDTEMPBED
+#define PIDTEMPBED
 //
 //#define BED_LIMIT_SWITCHING
 
@@ -211,9 +211,9 @@
 #ifdef PIDTEMPBED
 //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
 //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-    #define  DEFAULT_bedKp 10.00
-    #define  DEFAULT_bedKi .023
-    #define  DEFAULT_bedKd 305.4
+//    #define  DEFAULT_bedKp 10.00
+//    #define  DEFAULT_bedKi .023
+//    #define  DEFAULT_bedKd 305.4
 
 //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
 //from pidautotune
@@ -221,7 +221,12 @@
 //    #define  DEFAULT_bedKi 1.41
 //    #define  DEFAULT_bedKd 1675.16
 
-// FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
+//MonoPrice Mini
+//from pidautotune "M303 E-1 C8 S60" 
+    #define  DEFAULT_bedKp 274.44
+    #define  DEFAULT_bedKi 54.04
+    #define  DEFAULT_bedKd 348.48
+
 #endif // PIDTEMPBED
 
 
